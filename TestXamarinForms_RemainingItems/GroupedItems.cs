5ab93745346d0eed7d39477c34086f151e4ad6a8
Evert Kramer <e.kramer@fleetgo.com>
2019-11-05 15:46:11 +0100

    CollectionView RemainingItemsReached bug with grouped list

---
+++ TestXamarinForms_RemainingItems/GroupedItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestXamarinForms_RemainingItems
+{
+    public class GroupedItems : List<string>
+    {
+        public GroupedItems(IList<string> items) : base(items)
+        {
+            Name = $"Group {this.First()} - {this.Last()}";
+        }
+
+        public string Name { get; private set; }
+    }
+}
---
+++ TestXamarinForms_RemainingItems/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestXamarinForms_RemainingItems
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private static Lazy<MainPageViewModel> lazy = new Lazy<MainPageViewModel>(() => new MainPageViewModel());
+
+        public MainPageViewModel Instance => lazy.Value;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = Instance;
+        }
+
+        private int _remainingItemsThresholdReachedCounter;
+
+        private async void CollectionView_RemainingItemsThresholdReached(object sender, System.EventArgs e)
+        {
+            RemainingThresholdCount.Text = $"RemainingItemsThresholdReached {++_remainingItemsThresholdReachedCounter} time(s)";
+        }
+    }
+}
